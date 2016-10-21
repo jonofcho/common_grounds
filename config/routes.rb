3ce3bcc403af01a166e7a47fa30f8e645dbc38b7
jonathan Cho <jonathancho@jonathans-MacBook-Pro.local>
2016-10-21 05:38:45 -0700

    create checkout-3 / conf

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/checkout' => 'users#checkout'
   get '/checkout_2' => 'users#checkout_2'
   get '/checkout_3' => 'users#checkout_3'
+  get '/conf' => 'users#conf'
   get '/show' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
